8b801e60d62894055c45db7d2b4e38b0257800b6
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-06-16 21:22:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function threeSum(arr, target) {
-// write your code here
-  
+ let binary = "";
+  while (decimal > 0) {
+    binary = (decimal % 2) + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+  return binary;
 }
 
 module.exports = threeSum;
