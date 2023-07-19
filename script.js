fb23cc5fb862c3b376f520792fe865c17bf72b25
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-07-19 09:57:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function decimalToBinary(num) {
-  // Write your code here
+function decimalToBinary(decimal) {
+  return decimal.toString(2);
 }
+ 
+console.log(decimalToBinary(7));
+console.log(decimalToBinary(10)); 
+console.log(decimalToBinary(33)); 
 
 window.decimalToBinary = decimalToBinary;
